83a235b0cf9dbc8c5dfb390fcb75b45f1f4a7172
Leon Birch <leonbirch333@gmail.com>
2024-06-17 11:12:46 +0100

    added comments to see more clearly the code parts

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 'use srtict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check');
