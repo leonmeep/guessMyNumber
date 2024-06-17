7f1c1d001a3bce4af174ac006211cc4fab2497c1
Leon Birch <leonbirch333@gmail.com>
2024-06-17 12:02:27 +0100

    implemented first scenario - if input field is empty

--- script.js
+++ script.js
@@ -10,5 +10,10 @@
 // console.log(document.querySelector('.guess').value);
 
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+  }
 });
