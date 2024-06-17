dc0654f6073938d825a370ae132df68719ee04aa
Leon Birch <leonbirch333@gmail.com>
2024-06-17 16:14:12 +0100

    logic in place - error in high scores to be corrected

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
@@ -25,15 +16,27 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     /* if guess is too high*/
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
-    /*decrease score by 1*/
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      /*decrease score by 1*/
+      score--;
+      document.querySelector('.score').textContent = score;
+      /*if score is 0*/
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
     /* if guess is too low*/
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low!';
-    /*decrease score by 1*/
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      /*decrease score by 1*/
+      score--;
+      document.querySelector('.score').textContent = score;
+      /*if score is 0*/
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+    }
+    document.querySelector('.score').textContent = 0;
   }
 });
