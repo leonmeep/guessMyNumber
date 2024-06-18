973fa399fa740690a5b8edc4cacd0684876b3e9e
Leon Birch <leonbirch333@gmail.com>
2024-06-18 16:14:51 +0100

    implemented high score functionality

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-  /* if there is no number input*/
+  /* if there is no number input */
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
-    /* if player wins*/
+    /* if player wins */
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
@@ -18,13 +19,18 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('.number').style.width = '30rem';
 
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
     /* if guess is too high*/
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
       /*decrease score by 1*/
       score--;
-      document.querySelector('.score').textContent = score;
+
       /*if score is 0*/
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
@@ -37,6 +43,7 @@ document.querySelector('.check').addEventListener('click', function () {
       /*decrease score by 1*/
       score--;
       document.querySelector('.score').textContent = score;
+
       /*if score is 0*/
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
@@ -47,6 +54,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
+  let highscore = 0;
   const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
   document.querySelector('.message').textContent = 'Start guessing...';
