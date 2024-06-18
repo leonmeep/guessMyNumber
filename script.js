3f9ff0da946560615d9ea0757b1ac3db17b915da
Leon Birch <leonbirch333@gmail.com>
2024-06-18 13:03:54 +0100

    commented out score textContent = 0 on score too low statement

--- script.js
+++ script.js
@@ -37,6 +37,6 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
     }
-    document.querySelector('.score').textContent = 0;
+    // document.querySelector('.score').textContent = 0;
   }
 });
